7d3f301d49f006ff08faf11cb4a1e7335e734b08
VJHack <flymyplane21@gmail.com>
2022-06-16 19:52:21 -0500

    sign up and sign in

--- models/user.js
+++ models/user.js
@@ -9,6 +9,18 @@ const UserSchema = new mongoose.Schema ({
         type: String,
         required: true,
     },
+    email: {
+        type: String,
+        required: true
+    },
+    password: { 
+        type: String,
+        required: true 
+    },
+    gender: { 
+        type: String,
+        required: true 
+    },
     phoneNumber: {
         type: Number,
         required: true
--- routes/controller/users.js
+++ routes/controller/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 
 // Load Properies Model
 const User = require('../../models/user');
@@ -8,6 +11,110 @@ const User = require('../../models/user');
 
 
 //************************* USER CONTROLLER ***************************//
+
+
+// @route POST /users/signup
+// @description signup a user in the database
+// @access public
+router.post("/signup", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "User already has an account with this email"
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err
+            });
+          } else {
+            const user = new User({
+              //_id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash,
+              firstName: req.body.firstName,
+              lastName: req.body.lastName,
+              phoneNumber: req.body.phoneNumber,
+              age: req.body.age,
+              gender: req.body.gender
+            });
+            user
+              .save()
+              .then(result => {
+                console.log(result);
+                res.status(201).json({
+                  message: "User Sign Up Successful!"
+                });
+              })
+              .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                  error: err
+                });
+              });
+          }
+        });
+      }
+    });
+});
+
+
+
+// @route POST /users/login
+// @description login a user in the database and return access token
+// @access public
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Authentication Failed"
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Authentication Failed"
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id
+            },
+            process.env.JWT_KEY,
+            {
+                expiresIn: "1h"
+            }
+          );
+          return res.status(200).json({
+            message: "Authentication Successful",
+            accessToken: token
+          });
+        }
+        res.status(401).json({
+          message: "Authentication Failed"
+        });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+
+
+
+
+
 // @route GET /users
 // @description lists all of the users in the market
 // @access public
