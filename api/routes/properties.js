72c7772dd93d7979083ced3fbacce564227a39b0
VJHack <flymyplane21@gmail.com>
2022-06-24 19:42:48 -0500

    image resolution

--- api/routes/properties.js
+++ api/routes/properties.js
@@ -35,7 +35,7 @@ const storage = new GridFsStorage({
     },
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage, limits: { fieldSize: 25 * 1024 * 1024 } });
 
 //--------------------------------------------
 
