04ded7a89e560d96c224605e5eb6c5040527fab5
VJHack <flymyplane21@gmail.com>
2022-07-29 20:03:58 -0500

    reverse geocoding

--- api/routes/autocomplete.js
+++ api/routes/autocomplete.js
@@ -4,6 +4,6 @@ const AutocompleteController = require('../controller/autocomplete');
 
 router.get("/places/:query", AutocompleteController.places_autocomplete);
 router.get("/places/", AutocompleteController.places_autocomplete);
-router.get("/reversegeocoding/", AutocompleteController.places_autocomplete);
+router.get("/reversegeocoding/", AutocompleteController.reverse_geocoding);
 
 module.exports = router;
