5fd9ed09e2680cafea7cf2dbb24867986b5f7fe2
VJHack <flymyplane21@gmail.com>
2022-08-01 10:34:41 -0500

    removed password field

--- api/models/user.js
+++ api/models/user.js
@@ -13,10 +13,6 @@ const UserSchema = new mongoose.Schema ({
         type: String,
         required: true
     },
-    password: { 
-        type: String,
-        required: true 
-    },
     gender: { 
         type: String,
         required: true 
