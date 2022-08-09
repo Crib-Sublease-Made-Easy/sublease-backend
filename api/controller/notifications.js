fcae6272359e3d59f624e683c916324df08fd8cc
VJHack <flymyplane21@gmail.com>
2022-08-08 23:30:50 -0500

    able to send notif

--- api/controller/notifications.js
+++ api/controller/notifications.js
@@ -1,6 +1,48 @@
-const onesignal = require('api')('@onesignal/v9.0#e8ds50l6axnfm1');
+// const onesignal = require('api')('@onesignal/v9.0#e8ds50l6axnfm1');
 const User = require('../models/user');
+const request = require('request');
+require('dotenv').config();
+const API_KEY = 'NzFhOTU3MDQtMTkyZi00NDIxLWE5ZjQtNTU0YzRhMWIzNWIw';
+const ONESIGNAL_APP_ID = "440ad232-b229-4ea1-963b-5037d3ac9413";
+const BASE_URL = "https://onesignal.com/api/v1";
 
+/**
+ * OPTIONS BUILDER
+ * @param {string} method
+ * @param {string} path
+ * @param {object} body
+ * @returns {object} options
+ */
+const optionsBuilder = (method, path, body) => {
+    return {
+        method,
+        'url': `${BASE_URL}/${path}`,
+        'headers': {
+            'Content-Type': 'application/json',   
+            'Authorization': `Basic ${API_KEY}`,
+        },
+        body: body ? JSON.stringify(body) : null,
+    };
+}
+
+/**
+ * CREATE A PUSH NOTIFICATION
+ * method: POST
+ * Postman: https://www.postman.com/onesignaldevs/workspace/onesignal-api/request/16845437-c4f3498f-fd80-4304-a6c1-a3234b923f2c
+ * API Reference: https://documentation.onesignal.com/reference#create-notification
+ * path: /notifications
+ * @param {object} body
+ */
+
+const createNotication = (body) => {
+    const options = optionsBuilder("POST","notifications", body);
+    console.log(options);
+    request(options, (error, response) => {
+        if (error) throw new Error(error);
+        console.log(response.body);
+        // viewNotifcation(JSON.parse(response.body).id);
+    });
+}
 // @route POST
 // @description Send a notification for a chat message
 // @access Private
@@ -13,31 +55,28 @@ exports.send_message = async (req, res, next) => {
 
     if(senderId == part1){
       recipient = part2
-    } else if (senderId == part2){
+    } else{
       recipient = part1
     }
-  
+    console.log(recipient)
     await User.findById(recipient).then(user =>{
       console.log("INSIDE ")
-      onesignal.createNotification({
+      console.log(user.oneSignalUserId)
+      const body = {
+        app_id: ONESIGNAL_APP_ID,
         include_player_ids: [user.oneSignalUserId],
-          contents: {
-            en: req.body.message,
-          },
-          name: 'CRIB_CHAT'
-        }, {
-          authorization: 'ZGQ0ODYzMDAtYjEzMy00ZjUyLTkxZmEtYzUzMTFiNGFmYmMz'
-        })
-        console.log("TRANSFERING MESSAGE RETURN")
-
-       res.json({status: "Notification Successfully Sent"})
+        contents: {
+          en: 'New message from ' + user.firstName,
+        },
+        };
+    
+      createNotication(body);
+      console.log("TRANSFERING MESSAGE RETURN")
+      res.json({status: "Notification Successfully Sent"})
 
     }).catch(Exception=>
       res.status(404).json({ error: 'No such uuser' })
     )
-
-
-
 }; 
   
 
