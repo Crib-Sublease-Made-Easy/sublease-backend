24985c43535b2a87876b978bc268ee57382e1fee
isaaclhy13 <hlee777@wisc.edu>
2022-11-30 22:21:21 -0600

    Update autocomplete.js

--- api/controller/autocomplete.js
+++ api/controller/autocomplete.js
@@ -99,7 +99,12 @@ exports.geocoding_all = (req, res, next) => {
     .then(function (response) {
         let JSONdata = response.data
         console.log(JSONdata)
-        res.json(JSONdata.results[0])      
+        if(JSONdata.results.length() > 0){
+             res.json(JSONdata.results[0])    
+        }
+        else{
+             res.status(500).json({ error: 'invalid query' }) 
+        }
     })
     .catch(function (error) {
         console.log(error);
