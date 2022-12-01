15aca2ea64eb7375ff42c404edef130493a882f8
VJHack <flymyplane21@gmail.com>
2022-11-30 19:47:57 -0600

    changed autocomplete router

--- api/routes/autocomplete.js
+++ api/routes/autocomplete.js
@@ -6,7 +6,7 @@ router.get("/places/:query", AutocompleteController.places_autocomplete);
 router.get("/places/", AutocompleteController.places_autocomplete);
 router.get("/reversegeocoding", AutocompleteController.reverse_geocoding);
 router.get("/geocoding", AutocompleteController.geocoding);
-router.get("/geocoding_new", AutocompleteController.geocoding_new);
+router.get("/geocoding_all", AutocompleteController.geocoding_all);
 
 
 module.exports = router;
