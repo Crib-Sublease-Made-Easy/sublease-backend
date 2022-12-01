c14999c95c7eee0cfbf79fa53a4e17ac8b7d04dd
isaaclhy13 <hlee777@wisc.edu>
2022-11-30 19:14:15 -0600

    Update autocomplete.js

--- api/routes/autocomplete.js
+++ api/routes/autocomplete.js
@@ -6,7 +6,7 @@ router.get("/places/:query", AutocompleteController.places_autocomplete);
 router.get("/places/", AutocompleteController.places_autocomplete);
 router.get("/reversegeocoding", AutocompleteController.reverse_geocoding);
 router.get("/geocoding", AutocompleteController.geocoding);
-router.get("/geocoding_new", AutocompleteController.geocoding_new);
+router.get("/geocoding_new/:address", AutocompleteController.geocoding_new);
 
 
 module.exports = router;
