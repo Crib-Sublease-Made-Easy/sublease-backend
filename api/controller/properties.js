aca5871132dd235054a18ffbcec7d9ea417f6e78
isaaclhy13 <hlee777@wisc.edu>
2022-12-07 00:28:50 -0600

    Update properties.js

--- api/controller/properties.js
+++ api/controller/properties.js
@@ -512,7 +512,7 @@ exports.property_pins = (req, res, next) => {
 
   query.deleted = false
   console.log("PINSQUERY", query)
-  Property.find(query, '_id loc price imgList availableFrom availableTo bed bath postedBy title')
+  Property.find(query, '_id loc price imgList availableFrom availableTo bed bath postedBy title amenities')
     .then(proprties => res.json(proprties))
     .catch(err => res.status(404).json({ propertiesFound: 'none', error: err }));
 
