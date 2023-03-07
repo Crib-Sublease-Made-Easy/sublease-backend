9c692146a483f46ba2b00be08ff89e7ee77070a0
VJHack <flymyplane21@gmail.com>
2023-03-07 16:28:37 -0600

    Getting Aslam his token

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -8,6 +8,7 @@ module.exports = async (req, res, next) => {
         const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, process.env.JWT_KEY);
         req.userData = decoded;
+        console.log(token)
         await User.findByIdAndUpdate(decoded.userId, {
             lastActive: new Date(),
         });
