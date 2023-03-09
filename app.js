889a3639bf1d2e47cb6540ce9850ba9ca3bae387
isaaclhy13 <hlee777@wisc.edu>
2023-03-08 22:40:16 -0600

    change back:

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const corsOptions ={
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
