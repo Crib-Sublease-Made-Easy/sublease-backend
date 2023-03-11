9784c48963a9afddab053126b7abeb3bc18a1148
isaaclhy13 <hlee777@wisc.edu>
2023-03-10 23:13:34 -0600

    authy

--- api/controller/lead.js
+++ api/controller/lead.js
@@ -1,6 +1,8 @@
 const Lead = require('../models/lead');
 const mongoose = require("mongoose");
 const jwt = require('jsonwebtoken');
+const client = require('twilio')("ACe94558aef30193e8601dff1ab409c400", authToken);
+
 
 
 
@@ -27,21 +29,43 @@ exports.collect_leads = (req, res, next) => {
   }
 };
 
+// @route POST /leads
+// @description post leads
+// @access Public
+exports.ios_leads = (req, res, next) => {
+  if(req.body.number == undefined){
+    res.status(400).json({ error: 'Unable to send contact', errRaw: err });
+  } else{
+    client.messages
+    .create({
+        body: 'With Crib, sublease your apartment in just 30 seconds! Download the mobile app to get notified right away when others are interested in your sublease! https://apps.apple.com/us/app/crib-subleasing-made-easy/id1645127110',
+        from: '+18775226376',
+        to: `+1${req.body.number}`
+    })
+    .then(async (cont) => {
+      res.status(200).json({ msg: 'Email Stored Successfully' })
+    })
+    .done()
+    .catch(err => res.status(400).json({ error: 'Unable to store email', errRaw: err }));
+  }
+};
+
 
 // @route GET /leads
 // @description get leads
 // @access Public
 exports.get_leads = (req, res, next) => {
 
-    Lead.find()
-    .then(leads => {
-        res.json(
-            {
-                count: leads.length,    
-                leads
-            })
-        }
-    )
-    .catch(err => res.status(404).json({ leadsFound: 'none' }));
-    
-  };
+  Lead.find()
+  .then(leads => {
+      res.json(
+          {
+              count: leads.length,    
+              leads
+          })
+      }
+  )
+  .catch(err => res.status(404).json({ leadsFound: 'none' }));
+  
+};
+
