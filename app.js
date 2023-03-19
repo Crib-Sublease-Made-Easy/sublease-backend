724df32af5a789fd7a3d9bb75bc3523130045ef8
isaaclhy13 <hlee777@wisc.edu>
2023-03-19 18:36:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express();
 
 // connect database
 connectDB();
-app.use(express.json({limit: "50mb"}));
+app.use(express.json({limit: "5mb"}));
+app.use(express.urlencoded({limit: "5mb"}));
 
 // const corsOptions ={
 //     origin:'*', 
