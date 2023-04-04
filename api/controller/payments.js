23452f563746d35f1688749385c677130ccaa9d5
isaaclhy13 <hlee777@wisc.edu>
2023-04-04 15:57:09 -0500

    testing payment api

--- api/controller/payments.js
+++ api/controller/payments.js
@@ -23,6 +23,20 @@ var data = {
 	}
 }
 
+
+var testData = {
+    "description": "Crib Connect immediately connects tenants with reliable and interested subtenants at a very small fee $9.99. With Crib Connect you will receive a list of 5 to 10 potential subtenants.",
+	"quick_pay":{
+		"name": "Crib Connect",
+		"price_money": {
+			"amount": 100,
+			"currency": "USD"
+		},
+		"location_id": "LGZXV3FXE9F2J"
+
+	}
+}
+
 // @route POST /payments/premium/generatelink
 // @description generate payment link
 // @access Private
@@ -54,6 +68,67 @@ exports.prem_generate_link = async(req, res, next) => {
             'Authorization': 'Bearer ' + sq_access_token
         }, 
         body: JSON.stringify(data)
+      }).then(resp => resp.json())
+      .then(square_res => {
+        //   console.log("THE SQUARE RESPONSE", square_res)
+        const userId = decoded.userId;
+        if (userId == req.body.userId) {
+            
+            let query = {};
+            let cribPremium = {};
+            // console.log(square_res.related_resources.orders[0].state)
+            if (req.body.referralCode != undefined) {
+                query.referralCode = req.body.referralCode;
+                let paymentDetails = {};
+                if(square_res.related_resources.orders[0].state == "OPEN"){
+                    paymentDetails.status = true
+                }
+                else{
+                    paymentDetails.status = false
+                }
+                if(square_res.payment_link != undefined){
+                    paymentDetails.orderId = square_res.payment_link.order_id;
+                    paymentDetails.paymentLink = square_res.payment_link.url;
+                    paymentDetails.paymentLinkCreatedAt= square_res.payment_link.created_at;
+                    paymentDetails.paymentLinkId = square_res.payment_link.id;
+                }
+                cribPremium.paymentDetails = paymentDetails;
+                cribPremium.referred = [];
+            }
+            query.cribPremium = cribPremium;
+
+            console.log("Update user")
+
+            User.findByIdAndUpdate(userId, query)
+            // .then((user) => res.json(user))
+            .catch((err) =>
+                res.status(400).json({ error: "Unable to update the Database" })
+            );
+            return res.status(200).json(square_res)
+        }
+        else{
+            return res.status(400).json({
+                message: "Incomplete info",
+            });
+        }
+        
+        }
+      )
+      .catch(err => res.status(400).json({ error: 'unable to make request', errRaw: err }));
+};
+
+exports.prem_generate_testing_link = async(req, res, next) => {
+    console.log("HELLLOOOOOOOOOO")
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    await fetch("https://connect.squareup.com/v2/online-checkout/payment-links", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Square-Version': '2023-03-15',
+            'Authorization': 'Bearer ' + sq_access_token
+        }, 
+        body: JSON.stringify(testData)
       }).then(resp => resp.json())
       .then(square_res => {
         //   console.log("THE SQUARE RESPONSE", square_res)
--- api/routes/payments.js
+++ api/routes/payments.js
@@ -4,6 +4,8 @@ const PaymentsController = require('../controller/payments');
 const checkAuth = require("../middleware/check-auth");
 
 router.post("/premium/generatelink", checkAuth, PaymentsController.prem_generate_link);
+router.post("/premium/generatetestinglink", checkAuth, PaymentsController.prem_generate_testing_link);
+
 router.post("/premium/status", checkAuth, PaymentsController.prem_status);
 
 
