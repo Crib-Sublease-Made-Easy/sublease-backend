0f40bdf113c86eeb103e834ffbb1eb1dee686dd7
Mohamed Maazin Sudheer <mohammed.maazin@gmail.com>
2023-04-27 15:39:57 -0700

    env vars

--- api/controller/automation.js
+++ api/controller/automation.js
@@ -5,10 +5,6 @@ const User = require("../models/user");
 const Property = require('../models/property');
 const { response } = require("express");
 
-const IGID = "17841457359703661";
-const IGTOKEN = "EAAMoc0mnE3EBAM6oqriGt8P1yvBTpLMlZCVkeKFZCbzUAR57un6woEQZAlSK0SONwZBiqTKvy5mmJZC53ZC7xNK5EKWpEyB2Uh8rsxImA7nnHZBd6K5Cwu4YkJKeG57fQMMu4hmDBhUZBOcJdIVqABd3sqXR29CbsNlKGzQ5sudEnt0v5RWkixkT";
-
-
 exports.automate_instagram = (req, res, next) => {
     User.find({ phoneNumber: req.body.phoneNumber })
         .then(async (user) => {
@@ -20,8 +16,8 @@ exports.automate_instagram = (req, res, next) => {
                     // Create container for each image
                     for (var i=0; i < images.length; i++) {
                         let containerId;
-                        await fetch("https://graph.facebook.com/v16.0/" + IGID + 
-                            "/media?is_carousel_item=true&image_url=" + images[i] + "&access_token=" + IGTOKEN, 
+                        await fetch("https://graph.facebook.com/v16.0/" + IG_ID + 
+                            "/media?is_carousel_item=true&image_url=" + images[i] + "&access_token=" + IG_TOKEN, 
                         {
                             method: 'POST',
                             headers: {
@@ -46,8 +42,8 @@ exports.automate_instagram = (req, res, next) => {
                     description = "%24" + prop.price + "%20%2Fmo.%20" + start + "-" + end + "%20%7C%20" + "Property%20details%20on%20the%20Crib%20App!%20Contact%20608-515-8038%20if%20interested."
                     let carouselId;
 
-                    await fetch("https://graph.facebook.com/v16.0/" + IGID + 
-                        "/media?media_type=CAROUSEL&children=" + children + "&access_token=" + IGTOKEN, 
+                    await fetch("https://graph.facebook.com/v16.0/" + IG_ID + 
+                        "/media?media_type=CAROUSEL&children=" + children + "&access_token=" + IG_TOKEN, 
                     {
                         method: 'POST',
                         body: JSON.stringify({
@@ -71,8 +67,8 @@ exports.automate_instagram = (req, res, next) => {
                     
                     // Create post on Instagram
                     let postId;
-                    await fetch("https://graph.facebook.com/v16.0/" + IGID + 
-                        "/media_publish?creation_id=" + carouselId + "&access_token=" + IGTOKEN,
+                    await fetch("https://graph.facebook.com/v16.0/" + IG_ID + 
+                        "/media_publish?creation_id=" + carouselId + "&access_token=" + IG_TOKEN,
                     {
                         method: 'POST',
                         headers: {
