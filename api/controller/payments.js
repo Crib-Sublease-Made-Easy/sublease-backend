d0f0b839f3a667ed49a130ef2c8657a38a37b4f0
VJHack <flymyplane21@gmail.com>
2023-05-18 19:21:56 -0500

    new access token

--- api/controller/payments.js
+++ api/controller/payments.js
@@ -257,19 +257,7 @@ exports.prem_status = async(req, res, next) => {
                             })
                         }
                         console.log("IDS: " + String(fb_img_ids)) 
-                        let msg =  `Location: `+ String(p.loc.streetAddr)+`, `+ String( p.loc.secondaryTxt) + `
-
-Availability:` + (new Date(p.availableFrom)).toDateString() + ` - ` +  (new Date(p.availableTo)).toDateString() + `
-
-Price: $`+ String(p.price)+ `
-
-Type:  ` + String(p.type)+  `
-
-Rent is negotiable! 
-
-` + String(p.description) + `
-
-If you're interested, message me at: (608) 515-8038 with your name and this location. Thanks!`
+                        let msg =  "Location: "+ String(p.loc.streetAddr)+", "+ String( p.loc.secondaryTxt) + "\n\nAvailability: " + (new Date(p.availableFrom)).toDateString() + " - " +  (new Date(p.availableTo)).toDateString() + "\n\nPrice: $"+ String(p.price)+ "\n\nType:  " + String(p.type)+  "\n\nRent is negotiable!\n\n" + String(p.description) + "\n\nIf you're interested, message me at: (608) 515-8038 with your name and this location. Thanks!"
                         let url_post= "https://graph.facebook.com/v16.0/418408305947254/feed?"
                         console.log("ADDING IMAGES")
 
