92064e34ffd1a44100ef8c78ddc43847754e2abd
isaaclhy13 <hlee777@wisc.edu>
2023-05-31 15:32:25 -0400

    status

--- api/controller/properties.js
+++ api/controller/properties.js
@@ -1212,7 +1212,7 @@ exports.sublease_successful = async(req, res, next) => {
 // @route POST /properties/interenal/subleasedwithcomments
 // @description Update property
 // @access Public
-exports.sublease_successful_withcomments = (req, res, next) => {
+exports.sublease_successful_withcomments = async (req, res, next) => {
   if(req.body.success == undefined || req.body.comments == undefined || req.body.propId == undefined){
     res.status(404).json({data: "Error"})
   }
