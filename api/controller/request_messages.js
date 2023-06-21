5fd825af74e23d9b65c32443d875869ee905aadd
VJHack <flymyplane21@gmail.com>
2023-06-21 00:33:35 -0400

    request messaging feature

--- api/controller/request_messages.js
+++ api/controller/request_messages.js
@@ -1,29 +1,35 @@
 const RequestMessages = require('../models/request_messages');
 const mongoose = require("mongoose");
 const jwt = require('jsonwebtoken');
+const User = require("../models/user");
 
 
 
 
-//************************* PROPERTY CONTROLLER ***************************//
+//************************* REQUEST MESSAGE CONTROLLER ***************************//
 
 
 // @route POST /request_messages
 // @description send a message to the request
 // @access Private
 exports.send_messages = (req, res, next) => {
-    const message = new RequestMessages({
-        requestId: new mongoose.Types.ObjectId(req.body.requestId),
-        senderId: new mongoose.Types.ObjectId(req.body.senderId),
-        senderName: req.body.senderName,
-        timeStamp: new Date(),
-        message: req.body.message
-    })
-    message.save().then(r=>{
-        res.status(200).json({result: "messsage successfully sent"})
+    User.findOne({_id: new mongoose.Types.ObjectId(req.body.senderId)}).then(re=>{
+        const message = new RequestMessages({
+            requestId: new mongoose.Types.ObjectId(req.body.requestId),
+            senderId: new mongoose.Types.ObjectId(req.body.senderId),
+            senderName: re.firstName,
+            timeStamp: new Date(),
+            message: req.body.message
+        })
+        message.save().then(r=>{
+            res.status(200).json({result: "messsage successfully sent"})
+        }).catch(err=>{
+            res.status(400).json({err: "something went wrong", err})
+        })
     }).catch(err=>{
-        res.status(400).json({err: "something went wrong", err})
-    })
+            res.status(400).json({err: "something went wrong", err})
+        })
+
 };
 
 
